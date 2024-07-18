7b910066e2cc51ee4440abb5830cb8e6eccfd4c1
Aatir04 <148206095+Aatir04@users.noreply.github.com>
2024-07-18 16:37:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+num=int(input("enter your age"))
+if num%2==0:
+    print("Number is Even")
+else:
+    print("Number is Odd")
+
+
+
+num=int(input("enter your age"))
+if num%5==0:
+        print ("hello")
+else:
+       print("bye")
+
+
+amt =0
+nu=int(input("enter number of electric unit"))
+if nu<=100:
+ amt =0
+if nu>100 and nu<=200:
+     amt=(nu-100)*5
+if nu>200:
+         amt=500+(nu-200)*10
+         print("amount to pay:",amt)
+
+num=int(input("enter any number"))
+ld=num%10
+if ld%3==0:
+    print("last digit of number is divisible by 3")
+else:
+    print("last digit of number is not divisible by 3")
+
+
+
